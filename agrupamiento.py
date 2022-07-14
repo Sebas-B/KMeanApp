4458b29f2e4629ab6c581b0bc1a0917c34a7110c
Sebas-B <66041490+Sebas-B@users.noreply.github.com>
2022-07-14 11:03:03 -0500

    Se agrega agrupamiento.py

--- agrupamiento.py
+++ agrupamiento.py
@@ -2,32 +2,71 @@
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.cluster import KMeans
 
 # Leer usuario 
-data = pd.read_csv('')
-X = np.array(data)
+respuesta = pd.read_csv('./static/zodiacN.csv')
+X = np.array(respuesta)
 
-# Consstantes del sistema
-number_of_clusters = 6
+features = ["confianza","optimista","atencion","afecto","extrovertida","inteligente","fiesta","fisico","ejercicio","solitaria","viajar","estacion","emprendedor","pesimista","introvertida","elemento"]
 
-#Aplicar el algoritmo Kmeans a DATA
-kmeans = KMeans(
-	n_clusters=6,
-	init='k-means++',
-	n_init=10,
-    max_iter=300,
-	random_state=2
-	)
+respuesta = respuesta.dropna(subset=features)
 
-kfit = kmeans.fit(X)
-print(kfit)
+data = respuesta[features].copy()
 
-freeze_centroids = kmeans.cluster_centers_
-print(freeze_centroids)
-print(freeze_centroids.shape)
+data = (data - data.min()) / (data.max() - data.min()) * 9 + 1
+
+def random_centroids(data, k):
+    centroids = []
+    for i in range(k):
+        centroid = data.apply(lambda x: float(x.sample()))
+        centroids.append(centroid)
+    return pd.concat(centroids, axis=1) 
+
+centroids = random_centroids(data, 4)
+
+def get_labels(data, centroids):
+    distances = centroids.apply(lambda x: np.sqrt(((data - x)**2).sum(axis=1)))
+    return distances.idxmin(axis=1)
+
+labels = get_labels(data, centroids)
+
+def new_centroids(data, labels, k):
+    return data.groupby(labels).apply(lambda x: np.exp(np.log(x).mean())).T
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+def plot_clusters(data, labels, centroids, iteration):
+    pca = PCA(n_components=2)
+    data_2d = pca.fit_transform(data)
+    centroids_2d = pca.transform(centroids.T)
+    clear_output(wait=True)
+    plt.title(f'Numero de iteracion: {iteration}')
+    plt.scatter(x=data_2d[:,0], y=data_2d[:,1], c=labels, label=labels )
+    plt.scatter(x=centroids_2d[:,0], y=centroids_2d[:,1], c="red", marker ='*', label='centroides')
+    plt.grid(True)
+    plt.show()
+
+max_iterations = 10
+k = 4
+ 
+centroids = random_centroids(data, k)
+old_centroids = pd.DataFrame()
+iteration = 1
+
+while iteration < max_iterations and not centroids.equals(old_centroids):
+    old_centroid = centroids
+    
+    labels = get_labels(data, centroids)
+    centroids = new_centroids(data, labels, k)
+    plot_clusters(data, labels, centroids, iteration)
+    iteration += 1
+
+print(centroids)
+print(centroids.shape)
 
 # Salvar los centroides como un archivo pickle localmente
 with open('freezed_centroids.pkl','wb') as f:
-    pickle.dump(freeze_centroids, f)
+    pickle.dump(centroids, f)
     print('Los centroides fueron guardados como un archivo pickle localmente.')
--- app.py
+++ app.py
@@ -1,74 +1,115 @@
+import json
+import pickle
+from scipy.spatial import distance
 from flask import Flask
-from flask import render_template,request,redirect
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
+import numpy as np
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_DB']='horóscopos'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'zodiacales'
 mysql.init_app(app)
+
 
 @app.route('/')
 def index():
-
-    sql="SELECT * FROM `respuestas`;"
-    conn= mysql.connect()
-    cursor=conn.cursor()
+    sql = "SELECT * FROM `respuestas`;"
+    conn = mysql.connect()
+    cursor = conn.cursor()
     cursor.execute(sql)
 
-    respuestas=cursor.fetchall()
+    respuestas = cursor.fetchall()
     print(respuestas)
-    
+
     conn.commit()
-    return render_template('respuestas/index.html', respuestas=respuestas )
+    return render_template('respuestas/index.html', respuestas=respuestas)
+
 
 @app.route('/destroy/<int:id>')
 def destroy(id):
-    conn= mysql.connect()
-    cursor=conn.cursor()
-    
-    cursor.execute("DELETE FROM respuestas WHERE id_respuesta=%s",(id))
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM respuestas WHERE id_respuesta=%s", (id))
     conn.commit()
     return redirect('/')
 
+
 @app.route('/create')
 def create():
-
     return render_template('respuestas/create.html')
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'GET':
+        return 'La url /predict esta siendo accesada directamentamente. Por favor dirigete a la pagina principal'
+
+    if request.method == 'POST':
+        input_val = request.form
+
+        if input_val != None:
+            # collecting values
+            vals = []
+            for key, value in input_val.items():
+                vals.append(float(value))
+
+        # Calculate Euclidean distances to freezed centroids
+        with open('freezed_centroids.pkl', 'rb') as file:
+            freezed_centroids = pickle.load(file)
+
+        assigned_clusters = []
+        l = []  # Lista de distancias
+
+        for i, this_segment in enumerate(freezed_centroids):
+            dist = distance.euclidean(*vals, this_segment)
+            l.append(dist)
+            index_min = np.argmin(l)
+            assigned_clusters.append(index_min)
+
+        return render_template(
+            'predict.html', result_value=f'Segmento Calculado = # {index_min}'
+        )
+
 
 @app.route('/store', methods=['POST'])
 def storage():
+    _optimista = request.form['optimistaR']
+    _pesimista = request.form['pesimistaR']
+    _confianza = request.form['confianzaR']
+    _atencion = request.form['atencionR']
+    _afecto = request.form['afectoR']
+    _extrovertida = request.form['extrovertidaR']
+    _introvertida = request.form['introvertidaR']
+    _inteligente = request.form['inteligenteR']
+    _deprime = request.form['deprimeR']
+    _fiesta = request.form['fiestaR']
+    _fisico = request.form['fisicoR']
+    _ejercicio = request.form['ejercicioR']
+    _solitaria = request.form['solitariaR']
+    _viajar = request.form['viajarR']
+    _estacion = request.form['estacionR']
+    _emprendedor = request.form['emprendedorR']
+    _elemento = request.form['elementoR']
 
-    _optimista=request.form['optimistaR']
-    _pesimista=request.form['pesimistaR']
-    _confianza=request.form['confianzaR']
-    _atencion=request.form['atencionR']
-    _afecto=request.form['afectoR']
-    _extrovertida=request.form['extrovertidaR']
-    _introvertida=request.form['introvertidaR']
-    _inteligente=request.form['inteligenteR']
-    _deprime=request.form['deprimeR']
-    _fiesta=request.form['fiestaR']
-    _fisico=request.form['fisicoR']
-    _ejercicio=request.form['ejercicioR']
-    _solitaria=request.form['solitariaR']
-    _viajar=request.form['viajarR']
-    _estacion=request.form['estacionR']
-    _emprendedor=request.form['emprendedorR']
-    _elemento=request.form['elementoR']
-    
-    sql="INSERT INTO `respuestas` (`id_respuesta`, `optimistaR`, `pesimistaR`, `confianzaR`, `atencionR`, `afectoR`, `extrovertidaR`, `introvertidaR`, `inteligenteR`, `deprimeR`, `fiestaR`, `fisicoR`, `ejercicioR`, `solitariaR`, `viajarR`, `estacionR`, `emprendedorR`, `elementoR`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    
-    datos=(int(_optimista),int(_pesimista),int(_confianza),int(_atencion),int(_afecto),int(_extrovertida),int(_introvertida),int(_inteligente),int(_deprime),int(_fiesta),int(_fisico),int(_ejercicio),int(_solitaria),int(_viajar),int(_estacion),int(_emprendedor),int(_elemento))
+    sql = "INSERT INTO `respuestas` (`id_respuesta`, `optimistaR`, `pesimistaR`, `confianzaR`, `atencionR`, `afectoR`, `extrovertidaR`, `introvertidaR`, `inteligenteR`, `deprimeR`, `fiestaR`, `fisicoR`, `ejercicioR`, `solitariaR`, `viajarR`, `estacionR`, `emprendedorR`, `elementoR`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
-    conn= mysql.connect()
-    cursor=conn.cursor()
-    cursor.execute(sql,datos)
+    datos = (int(_optimista), int(_pesimista), int(_confianza), int(_atencion), int(_afecto), int(_extrovertida), int(_introvertida), int(_inteligente), int(
+        _deprime), int(_fiesta), int(_fisico), int(_ejercicio), int(_solitaria), int(_viajar), int(_estacion), int(_emprendedor), int(_elemento))
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
     conn.commit()
-    return redirect('respuestas/predict.html')
+    val = datos
+    post = request.post('/predict', json = val)
+    return post
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
